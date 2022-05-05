1a5d8db75b12e37369e7ae3a3f0724ccdceb3b46
Sergio <sergiquijano@gmail.com>
2022-05-05 20:29:33 +0200

    feat #1: marco los campos que son obligatorios para crear un usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+
+# Modelo para la clase Usuario
 class User < ApplicationRecord
+
+  # Todos los campos del formulario son obligatorios salvo el email
+  validates_presence_of :username
+  validates_presence_of :password
+  validates_presence_of :role
+
 end
